25806bf22cf50a49e854e2fb5a89f52d669e9bac
Shijie Ma <mashijie2021@ia.ac.cn>
2024-03-06 15:55:48 +0800

    Add files via upload

---
+++ utils_simgcd/cluster_and_log_utils_al.py
@@ -0,0 +1,184 @@
+import torch
+#import torch.distributed as dist
+import numpy as np
+from scipy.optimize import linear_sum_assignment as linear_assignment
+
+
+# def all_sum_item(item):
+#     item = torch.tensor(item).cuda()
+#     dist.all_reduce(item)
+#     return item.item()
+
+def split_cluster_acc_v2(y_true, y_pred, mask):
+    """
+    Calculate clustering accuracy. Require scikit-learn installed
+    First compute linear assignment on all data, then look at how good the accuracy is on subsets
+
+    # Arguments
+        mask: Which instances come from old classes (True) and which ones come from new classes (False)
+        y: true labels, numpy.array with shape `(n_samples,)`
+        y_pred: predicted labels, numpy.array with shape `(n_samples,)`
+
+    # Return
+        accuracy, in [0,1]
+    """
+    y_true = y_true.astype(int)
+
+    old_classes_gt = set(y_true[mask])
+    new_classes_gt = set(y_true[~mask])
+
+    assert y_pred.size == y_true.size
+    D = max(y_pred.max(), y_true.max()) + 1
+    w = np.zeros((D, D), dtype=int)
+    for i in range(y_pred.size):
+        w[y_pred[i], y_true[i]] += 1
+
+    ind = linear_assignment(w.max() - w)
+    ind = np.vstack(ind).T
+
+    ind_map = {j: i for i, j in ind}
+    total_acc = sum([w[i, j] for i, j in ind])
+    total_instances = y_pred.size
+    # try: 
+    #     if dist.get_world_size() > 0:
+    #         total_acc = all_sum_item(total_acc)
+    #         total_instances = all_sum_item(total_instances)
+    # except:
+    #     pass
+    total_acc /= total_instances
+
+    old_acc = 0
+    total_old_instances = 0
+    for i in old_classes_gt:
+        old_acc += w[ind_map[i], i]
+        total_old_instances += sum(w[:, i])
+
+    # try:
+    #     if dist.get_world_size() > 0:
+    #         old_acc = all_sum_item(old_acc)
+    #         total_old_instances = all_sum_item(total_old_instances)
+    # except:
+    #     pass
+    old_acc /= total_old_instances
+
+    new_acc = 0
+    total_new_instances = 0
+    for i in new_classes_gt:
+        new_acc += w[ind_map[i], i]
+        total_new_instances += sum(w[:, i])
+
+    # try:
+    #     if dist.get_world_size() > 0:
+    #         new_acc = all_sum_item(new_acc)
+    #         total_new_instances = all_sum_item(total_new_instances)
+    # except:
+    #     pass
+    new_acc /= total_new_instances
+
+    return total_acc, old_acc, new_acc, ind_map
+
+
+def split_cluster_acc_v2_balanced(y_true, y_pred, mask):
+    """
+    Calculate clustering accuracy. Require scikit-learn installed
+    First compute linear assignment on all data, then look at how good the accuracy is on subsets
+
+    # Arguments
+        mask: Which instances come from old classes (True) and which ones come from new classes (False)
+        y: true labels, numpy.array with shape `(n_samples,)`
+        y_pred: predicted labels, numpy.array with shape `(n_samples,)`
+
+    # Return
+        accuracy, in [0,1]
+    """
+    y_true = y_true.astype(int)
+
+    old_classes_gt = set(y_true[mask])
+    new_classes_gt = set(y_true[~mask])
+
+    assert y_pred.size == y_true.size
+    D = max(y_pred.max(), y_true.max()) + 1
+    w = np.zeros((D, D), dtype=int)
+    for i in range(y_pred.size):
+        w[y_pred[i], y_true[i]] += 1
+
+    ind = linear_assignment(w.max() - w)
+    ind = np.vstack(ind).T
+
+    ind_map = {j: i for i, j in ind}
+
+    old_acc = np.zeros(len(old_classes_gt))
+    total_old_instances = np.zeros(len(old_classes_gt))
+    for idx, i in enumerate(old_classes_gt):
+        old_acc[idx] += w[ind_map[i], i]
+        total_old_instances[idx] += sum(w[:, i])
+
+    new_acc = np.zeros(len(new_classes_gt))
+    total_new_instances = np.zeros(len(new_classes_gt))
+    for idx, i in enumerate(new_classes_gt):
+        new_acc[idx] += w[ind_map[i], i]
+        total_new_instances[idx] += sum(w[:, i])
+
+    # try:
+    #     if dist.get_world_size() > 0:
+    #         old_acc, new_acc = torch.from_numpy(old_acc).cuda(), torch.from_numpy(new_acc).cuda()
+    #         dist.all_reduce(old_acc), dist.all_reduce(new_acc)
+    #         dist.all_reduce(total_old_instances), dist.all_reduce(total_new_instances)
+    #         old_acc, new_acc = old_acc.cpu().numpy(), new_acc.cpu().numpy()
+    #         total_old_instances, total_new_instances = total_old_instances.cpu().numpy(), total_new_instances.cpu().numpy()
+    # except:
+    #     pass
+
+    total_acc = np.concatenate([old_acc, new_acc]) / np.concatenate([total_old_instances, total_new_instances])
+    old_acc /= total_old_instances
+    new_acc /= total_new_instances
+    total_acc, old_acc, new_acc = total_acc.mean(), old_acc.mean(), new_acc.mean()
+    return total_acc, old_acc, new_acc, ind_map
+
+
+EVAL_FUNCS = {
+    'v2': split_cluster_acc_v2,
+    'v2b': split_cluster_acc_v2_balanced
+}
+
+def log_accs_from_preds(y_true, y_pred, mask, eval_funcs, save_name, T=None,
+                        print_output=True, args=None):
+
+    """
+    Given a list of evaluation functions to use (e.g ['v1', 'v2']) evaluate and log ACC results
+
+    :param y_true: GT labels
+    :param y_pred: Predicted indices
+    :param mask: Which instances belong to Old and New classes
+    :param T: Epoch
+    :param eval_funcs: Which evaluation functions to use
+    :param save_name: What are we evaluating ACC on
+    :param writer: Tensorboard logger
+    :return:
+    """
+
+    mask = mask.astype(bool)
+    y_true = y_true.astype(int)
+    y_pred = y_pred.astype(int)
+
+    for i, f_name in enumerate(eval_funcs):
+
+        acc_f = EVAL_FUNCS[f_name]
+        all_acc, old_acc, new_acc, ind_map = acc_f(y_true, y_pred, mask)
+        log_name = f'{save_name}_{f_name}'
+
+        if i == 0:
+            to_return = (all_acc, old_acc, new_acc, ind_map)
+
+        if print_output:
+            print_str = f'Epoch {T}, {log_name}: All {all_acc:.4f} | Old {old_acc:.4f} | New {new_acc:.4f}'
+            # try:
+            #     if dist.get_rank() == 0:
+            #         try:
+            #             args.logger.info(print_str)
+            #         except:
+            #             print(print_str)
+            # except:
+            #     pass
+
+    return to_return
---
+++ utils_simgcd/general_utils_al.py
@@ -0,0 +1,115 @@
+import os
+import torch
+import inspect
+
+from datetime import datetime
+from loguru import logger
+import time
+
+class AverageMeter(object):
+    """Computes and stores the average and current value"""
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+
+def init_experiment(args, runner_name=None, exp_id=None):
+    # Get filepath of calling script
+    if runner_name is None:
+        runner_name = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe()))).split(".")[-2:]
+
+    #root_dir = os.path.join(args.exp_root, *runner_name)
+    root_dir = os.path.join(args.exp_root, args.dataset_name)
+
+
+    if not os.path.exists(root_dir):
+        os.makedirs(root_dir)
+
+    now = args.base_ckpts_date
+    if args.base_exp_id is not None:
+        #now = now + '_' + args.base_exp_id
+        now = args.base_exp_id + '_' + now
+    now = now + '_query' + str(args.num_query)   # NOTE!!! add #query to log folder
+
+    # AL path dir
+    log_dir = os.path.join(root_dir, now, args.strategy)
+
+    if exp_id is not None:
+        log_dir = log_dir + '_' + exp_id
+
+    if not os.path.exists(log_dir):
+        os.makedirs(log_dir)
+
+
+    logger.add(os.path.join(log_dir, 'log.txt'))
+    args.logger = logger
+    args.log_dir = log_dir
+
+    # Instantiate directory to save models to
+    model_root_dir = os.path.join(args.log_dir, 'checkpoints')
+    if not os.path.exists(model_root_dir):
+        os.mkdir(model_root_dir)
+
+    args.model_dir = model_root_dir
+    args.model_path = os.path.join(args.model_dir, 'model.pt')
+
+    print(f'Experiment saved to: {args.log_dir}')
+
+    hparam_dict = {}
+
+    for k, v in vars(args).items():
+        if isinstance(v, (int, float, str, bool, torch.Tensor)):
+            hparam_dict[k] = v
+
+    print(runner_name)
+
+    # print and save args
+    print(args)
+    save_args_path = os.path.join(log_dir, 'args.txt')
+    f_args = open(save_args_path, 'w')
+    f_args.write('args: \n')
+    f_args.write(str(vars(args)))
+    f_args.close()
+
+    return args
+
+
+class DistributedWeightedSampler(torch.utils.data.distributed.DistributedSampler):
+
+    def __init__(self, dataset, weights, num_samples, num_replicas=None, rank=None,
+                 replacement=True, generator=None):
+        super(DistributedWeightedSampler, self).__init__(dataset, num_replicas, rank)
+        if not isinstance(num_samples, int) or isinstance(num_samples, bool) or \
+                num_samples <= 0:
+            raise ValueError("num_samples should be a positive integer "
+                             "value, but got num_samples={}".format(num_samples))
+        if not isinstance(replacement, bool):
+            raise ValueError("replacement should be a boolean value, but got "
+                             "replacement={}".format(replacement))
+        self.weights = torch.as_tensor(weights, dtype=torch.double)
+        self.num_samples = num_samples
+        self.replacement = replacement
+        self.generator = generator
+        self.weights = self.weights[self.rank::self.num_replicas]
+        self.num_samples = self.num_samples // self.num_replicas
+
+    def __iter__(self):
+        rand_tensor = torch.multinomial(self.weights, self.num_samples, self.replacement, generator=self.generator)
+        rand_tensor =  self.rank + rand_tensor * self.num_replicas
+        yield from iter(rand_tensor.tolist())
+
+    def __len__(self):
+        return self.num_samples
